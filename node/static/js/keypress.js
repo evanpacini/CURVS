86261f14a0e3677ae6792db3f0a46279dea19f89
Evan Pacini <evan.pacini@gmail.com>
2021-12-02 21:24:18 +0100

    bugfix tdm

--- node/static/js/keypress.js
+++ node/static/js/keypress.js
@@ -1,14 +1,14 @@
 var keys = [];
 var velocityHTML = document.getElementById("velocities");
-var tdm = true; // Tank drive mode
+var tdm = document.getElementById("tdm"); // Tank drive mode
 velocityHTML.innerHTML = "Velocities: 0, 0";
 // QASW
 // Q=left+ A=left- S=right- W=right+
 // WASD
 // W=thrust+ A=left S=thrust- D=right
-document
-  .getElementById("tdm")
-  .addEventListener("change", () => (tdm = this.checked));
+tdm.addEventListener("change", () => {
+  console.log("Tank drive mode: " + tdm.checked);
+});
 
 window.addEventListener(
   "keydown",
@@ -33,7 +33,7 @@ window.addEventListener(
 );
 
 function keysToSpeed(keys) {
-  if (tdm) {
+  if (tdm.checked) {
     var v1 = keys.includes("q") - keys.includes("a"); // left motor
     var v2 = keys.includes("w") - keys.includes("s"); // right motor
     return [v1, v2];
