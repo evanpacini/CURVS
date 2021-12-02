92223bd34b63d5661f48a5258cbe200f580487b4
Evan Pacini <evan.pacini@gmail.com>
2021-12-02 20:06:10 +0100

    keys to velocities conversion

--- node/static/js/keypress.js
+++ node/static/js/keypress.js
@@ -1,13 +1,23 @@
 var keys = [];
-var locus = document.getElementById("keypresses");
-locus.innerHTML = "Keys currently pressed: ";
+var velocityHTML = document.getElementById("velocities");
+var tdm = true; // Tank drive mode
+velocityHTML.innerHTML = "Velocities: 0, 0";
+// QASW
+// Q=left+ A=left- S=right- W=right+
+// WASD
+// W=thrust+ A=left S=thrust- D=right
+document
+  .getElementById("tdm")
+  .addEventListener("change", () => (tdm = this.checked));
+
 window.addEventListener(
   "keydown",
   function (e) {
     if (!keys.includes(e.key)) {
       keys.push(e.key);
     }
-    locus.innerHTML = "Keys currently pressed:" + keys.join(", ").toString();
+    velocityHTML.innerHTML =
+      "Velocities: " + keysToSpeed(keys).join(", ").toString();
   },
   false
 );
@@ -16,7 +26,20 @@ window.addEventListener(
   "keyup",
   function (e) {
     keys = keys.filter((item) => item !== e.key);
-    locus.innerHTML = "Keys currently pressed: " + keys.join(", ").toString();
+    velocityHTML.innerHTML =
+      "Velocities: " + keysToSpeed(keys).join(", ").toString();
   },
   false
 );
+
+function keysToSpeed(keys) {
+  if (tdm) {
+    var v1 = keys.includes("q") - keys.includes("a"); // left motor
+    var v2 = keys.includes("w") - keys.includes("s"); // right motor
+    return [v1, v2];
+  } else {
+    var v1 = keys.includes("w") - keys.includes("s"); // thrust
+    var v2 = keys.includes("d") - keys.includes("a"); // direction
+    return [v1, v2];
+  }
+}
